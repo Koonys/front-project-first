17ab80f19784f6760bac6f66c3f36fa1bd27c4ea
SeokHoon <sughoon1@gmail.com>
2024-02-13 02:06:56 +0900

    숫자 맞추기 게임 완성

--- main.js
+++ main.js
@@ -3,20 +3,39 @@ let playBtn = document.getElementById("playBtn");
 let userInput = document.getElementById("userInput");
 let result = document.getElementById("resultArea");
 let resetBtn = document.getElementById("resetBtn");
+let hintBtn = document.getElementById("hintBtn");
+let hintSpan = document.getElementById("hintMessage");
+let userHistory = document.getElementById("userHistory");
 let chance = 5;
 let gameOver = false;
 let chanceArea = document.getElementById("chanceArea");
 let history = [];
+let hintCount = 3;
+let difference2 = 0;
+let hintMessage="힌트가 필요 없어 보여요!";
+let randomRangeNum = randomRange();
 
 playBtn.addEventListener("click", play);
 resetBtn.addEventListener("click",reset);
+hintBtn.addEventListener("mouseenter",function(e){
+   hintMessage = hint(e);
+   if(hintCount==0){
+    hintSpan.innerText = hintMessage+"\n기회를 전부 소진 했어요";
+   }else if(hintCount==3){
+    hintSpan.innerText = "게임을 시작해 주세요";
+   }else{
+    hintSpan.innerText = hintMessage;
+   }
+});
+hintBtn.addEventListener("mouseleave", function(){
+    hintBtn.innerText = "HINT";
+});
 userInput.addEventListener("focus", function(){
     userInput.value="";
 });
 
 function randomNum(){
     setNum = Math.floor(Math.random()*100)+1;
-    console.log("정답 :"+setNum)
 }
 
 
@@ -24,14 +43,19 @@ function play(){
     let userVal = userInput.value;
 
     if(userVal<1||userVal>100){
-        result.textContent="1과100 사이의 숫자를 입력해주세요"
+        result.textContent="1과 100 사이의 숫자를 입력해주세요"
+        result.style.fontSize = "14px";
+        userInput.value="";
         return;
     }else if(history.includes(userVal)){
-        result.textContent="중복된 숫자를 입력하셨습니다 다른 숫자를 입력해주세요"
+        result.innerHTML="중복된 숫자를 입력하셨습니다.<br>다른 숫자를 입력해 주세요"
+        result.style.fontSize = "14px";
+        userInput.value="";
         return;
     }
     chance--;
     chanceArea.textContent=`남은기회 : ${chance}번`;
+    result.style.fontSize = "30px";
     if(userVal < setNum){
         result.textContent = "UP!!!"
     }else if(userVal > setNum){
@@ -42,7 +66,6 @@ function play(){
     }
 
     history.push(userVal);
-    console.log(history);
 
     if(chance < 1){
         gameOver = true;
@@ -50,17 +73,50 @@ function play(){
     if(gameOver == true){
         playBtn.disabled = true;
     }
+    let historyResult = "";
+    for(let i=0;i<history.length;i++){
+        historyResult += `<td>${history[i]}</td>`;
+    }
+    userHistory.innerHTML = historyResult;
+    userInput.value="";
 }
 
 function reset(){
     randomNum();
-    result.textContent = "결과출력";
+    result.textContent = "";
     userInput.value = "";
     playBtn.disabled = false;
     gameOver = false;
     chance = 5;
     chanceArea.textContent=`남은기회 : ${chance}번`;
     history = [];
+    hintCount = 2;
+    hintMessage = "힌트가 필요 없어 보여요!"
+}
+
+function hint(e){
+    if(hintCount==0){
+        e.preventDefault();
+        return hintMessage;
+    }
+    let userAnswer = history[history.length - 1];
+    let difference = Math.abs(setNum - userAnswer);
+    let hintRange = chance*10;
+    if(difference >= hintRange){
+        if(difference!==difference2){
+            difference2=difference
+            hintCount--;
+            randomRangeNum = randomRange();
+        }
+        let minRange = Math.max(0, setNum - hintRange-randomRangeNum);
+        let maxRange = Math.min(100, setNum + hintRange+randomRangeNum);
+        return `정답은 ${minRange}에서 ${maxRange}사이에 있습니다.`;
+    }
+    return "정답에 거의 근접했어요";
+}
+
+function randomRange(){
+    return Math.floor(Math.random()*10);
 }
 
 randomNum();
